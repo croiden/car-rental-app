f538a996d68b19722f2564c7755f756bc4d3658c
Lawrence Lobo <llobo@juniper.net>
2025-04-11 15:17:47 +0530

    RCA-7: Set up ESLint, Prettier, EditorConfig

--- eslint.config.js
+++ eslint.config.js
@@ -3,26 +3,33 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import prettier from 'eslint-config-prettier'
+import pluginPrettier from 'eslint-plugin-prettier'
 
 export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
+   {
+      ignores: ['dist'],
+   },
+   {
+      extends: [
+         js.configs.recommended,
+         ...tseslint.configs.recommended,
+         prettier, // ⬅ Disables ESLint rules that conflict with Prettier
       ],
-    },
-  },
+      files: ['**/*.{ts,tsx}'],
+      languageOptions: {
+         ecmaVersion: 2020,
+         globals: globals.browser,
+      },
+      plugins: {
+         'react-hooks': reactHooks,
+         'react-refresh': reactRefresh,
+         prettier: pluginPrettier, // ⬅ Adds Prettier as a plugin
+      },
+      rules: {
+         ...reactHooks.configs.recommended.rules,
+         'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+         'prettier/prettier': 'error', // ⬅ Runs Prettier as an ESLint rule
+      },
+   },
 )
