efde3d75558bd19d6d2600b5ad2694a3f74ccca4
Lawrence Lobo <llobo@juniper.net>
2025-04-11 14:39:26 +0530

    RCA-4: Add zustand useStore file for rental car

---
+++ src/store/useCarStore.ts
@@ -0,0 +1,11 @@
+import { create } from 'zustand'
+
+interface CarState {
+  selectedCarId: string | null
+  setSelectedCarId: (id: string | null) => void
+}
+
+export const useCarStore = create<CarState>((set) => ({
+  selectedCarId: null,
+  setSelectedCarId: (id) => set({ selectedCarId: id }),
+}))
