8bdaebe5df98f41e94ce218999a2d2e8adc887d0
Lawrence Lobo <llobo@juniper.net>
2025-04-11 14:34:41 +0530

    RCA-1: Configure theme and provider for chakra UI

---
+++ src/theme/index.ts
@@ -0,0 +1,15 @@
+import {
+    createSystem,
+    defaultConfig,
+    defineConfig,
+  } from "@chakra-ui/react"
+  
+  const config = defineConfig({
+    theme: {
+      tokens: {
+        colors: {},
+      },
+    },
+  })
+  
+export default createSystem(defaultConfig, config)
