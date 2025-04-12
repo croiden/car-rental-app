42a0af9d72dfbbb8b9fe35d3cebea5a683dbd566
Lawrence Lobo <llobo@juniper.net>
2025-04-12 19:12:10 +0530

    RCA-26: Add Chakra UI styling for vite based application

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { ChakraProvider } from '@chakra-ui/react'
+
+import { Provider } from '@/components/ui/provider'
+import { Toaster } from '@/components/ui/toaster'
 
 import './i18n'
-import theme from './theme'
 import App from './App.tsx'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
    <StrictMode>
-      <ChakraProvider value={theme}>
+      <Provider forcedTheme={'light'}>
          <App />
-      </ChakraProvider>
+         <Toaster />
+      </Provider>
    </StrictMode>,
 )
