8b598a26414cf8719313510eb7a4779c1d1da5ab
Lawrence Lobo <llobo@juniper.net>
2025-04-13 14:38:15 +0530

    RCA-420: Add page icon, empty icon and brand icons

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,18 @@
-import { Flex, Text } from '@chakra-ui/react'
+import { Flex, Icon, Text } from '@chakra-ui/react'
+import { IoCarSportSharp } from 'react-icons/io5'
 import { ColorModeButton } from '@/components/ui/color-mode'
 
 const NavBar = () => {
    return (
       <Flex bg="teal.700" p={4} justifyContent="space-between" alignItems="center">
-         <Text fontSize="lg" fontWeight="bold" color={'white'}>
-            Car Rental App
-         </Text>
+         <Flex alignItems="center" gap={4}>
+            <Icon size="xl" color="white">
+               <IoCarSportSharp />
+            </Icon>
+            <Text fontSize="lg" fontWeight="bold" color={'white'}>
+               Car Rental App
+            </Text>
+         </Flex>
          <ColorModeButton
             color={'white'}
             _hover={{
